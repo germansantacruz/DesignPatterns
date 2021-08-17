052b5fc0e2f8fba3859e775557eaa4bed951f4d4
Germán Santa Cruz <german.scf@hotmail.com>
2021-08-17 16:23:01 -0400

    ProductContext completed.

--- DesignPatterns.CQRSDemo/Context/ProductContext.cs
+++ DesignPatterns.CQRSDemo/Context/ProductContext.cs
@@ -20,10 +20,10 @@ namespace DesignPatterns.CQRSDemo.Context
         public async Task<int> Add(Product product)
         {
             Object commandExecutionResult = null;
-            var connection = new SqlConnection(_connectionString);
+            using var connection = new SqlConnection(_connectionString);
             try
             {
-                var command = connection.CreateCommand();
+                using var command = connection.CreateCommand();
                 command.CommandType = System.Data.CommandType.Text;
                 command.CommandText = "insert into Products (" +
                     $"{nameof(Product.Name)}, " +
@@ -36,7 +36,11 @@ namespace DesignPatterns.CQRSDemo.Context
                     $"{nameof(Product.Discontinued)}) " +
                     "values (@Name, @QuantityPerUnit, @Description, @UnitPrice, " +
                     "@UnitsInStock, @UnitsOnOrder, @ReorderLevel, @Discontinued);" +
-                    "select @@Identity";
+                    "select @@Identity;";
+
+                SetSqlParameters(command.Parameters, product);
+                await connection.OpenAsync();
+                commandExecutionResult = await command.ExecuteScalarAsync();
 
             }
             catch (Exception ex)
@@ -47,24 +51,148 @@ namespace DesignPatterns.CQRSDemo.Context
             return commandExecutionResult == null ? -1 : Convert.ToInt32(commandExecutionResult);
         }
 
-        public Task<IEnumerable<Product>> GetAll()
+        private void SetSqlParameters(SqlParameterCollection parameters, Product product)
         {
-            throw new NotImplementedException();
+            parameters.AddWithValue($"@{nameof(Product.Name)}", product.Name);
+            parameters.AddWithValue($"@{nameof(Product.QuantityPerUnit)}", product.QuantityPerUnit);
+            parameters.AddWithValue($"@{nameof(Product.Description)}", product.Description);
+            parameters.AddWithValue($"@{nameof(Product.UnitPrice)}", product.UnitPrice);
+            parameters.AddWithValue($"@{nameof(Product.UnitsInStock)}", product.UnitsInStock);
+            parameters.AddWithValue($"@{nameof(Product.UnitsOnOrder)}", product.UnitsOnOrder);
+            parameters.AddWithValue($"@{nameof(Product.ReorderLevel)}", product.ReorderLevel);
+            parameters.AddWithValue($"@{nameof(Product.Discontinued)}", product.Discontinued);
         }
 
-        public Task<Product> GetById(int id)
+        public async Task<IEnumerable<Product>> GetAll()
         {
-            throw new NotImplementedException();
+            List<Product> products = null;
+            var connection = new SqlConnection(_connectionString);
+            
+            try
+            {
+                using var command = connection.CreateCommand();
+                command.CommandType = System.Data.CommandType.Text;
+                command.CommandText = "select * from Products;";
+
+                await connection.OpenAsync();
+                var reader = await command.ExecuteReaderAsync();
+                if (reader != null)
+                {
+                    products = new List<Product>();
+                    while (await reader.ReadAsync())
+                    {
+                        products.Add(GetProduct(reader));
+                    }
+                    await reader.DisposeAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            await connection.DisposeAsync();
+            return products;
         }
 
-        public Task<bool> Remove(int id)
+        private Product GetProduct(SqlDataReader reader)
         {
-            throw new NotImplementedException();
+            return new Product
+            { 
+                Id = reader.GetInt32(reader.GetOrdinal(nameof(Product.Id))),
+                Name = reader.GetString(reader.GetOrdinal(nameof(Product.Name))),
+                QuantityPerUnit = reader.GetString(reader.GetOrdinal(nameof(Product.QuantityPerUnit))),
+                Description = reader.GetString(reader.GetOrdinal(nameof(Product.Description))),
+                UnitPrice = reader.GetDecimal(reader.GetOrdinal(nameof(Product.UnitPrice))),
+                UnitsInStock = reader.GetInt32(reader.GetOrdinal(nameof(Product.UnitsInStock))),
+                ReorderLevel = reader.GetInt32(reader.GetOrdinal(nameof(Product.ReorderLevel))),
+                Discontinued = reader.GetBoolean(reader.GetOrdinal(nameof(Product.Discontinued)))
+            };
         }
 
-        public Task<bool> Update(Product product)
+        public async Task<Product> GetById(int id)
         {
-            throw new NotImplementedException();
+            Product product = null;
+            var connection = new SqlConnection(_connectionString);
+
+            try
+            {
+                using var command = connection.CreateCommand();
+                command.CommandType = System.Data.CommandType.Text;
+                command.CommandText = "select * from Products where Id = @Id;";
+                command.Parameters.AddWithValue("@Id", id);
+
+                await connection.OpenAsync();
+                var reader = await command.ExecuteReaderAsync();
+                if (reader != null)
+                {
+                    await reader.ReadAsync();
+                    product = GetProduct(reader);
+                    await reader.DisposeAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            await connection.DisposeAsync();
+            return product;
+        }
+
+        public async Task<bool> Remove(int id)
+        {
+            Object commandExecutionResult = null;
+            var connection = new SqlConnection(_connectionString);
+            try
+            {
+                using var command = connection.CreateCommand();
+                command.CommandType = System.Data.CommandType.Text;
+                command.CommandText = "delete from Products where Id = @Id;";
+                command.Parameters.AddWithValue("@Id", id);                
+
+                await connection.OpenAsync();
+                commandExecutionResult = await command.ExecuteNonQueryAsync();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            await connection.DisposeAsync();
+            return commandExecutionResult == null ? false : ((int)commandExecutionResult == 1);
+        }
+
+        public async Task<bool> Update(Product product)
+        {
+            Object commandExecutionResult = null;
+            var connection = new SqlConnection(_connectionString);
+            try
+            {
+                using var command = connection.CreateCommand();
+                command.CommandType = System.Data.CommandType.Text;
+                command.CommandText = "Update Products set " +
+                    $"{nameof(Product.Name)} = @Name, " +
+                    $"{nameof(Product.QuantityPerUnit)} = @QuantityPerUnit, " +
+                    $"{nameof(Product.Description)} = @Description, " +
+                    $"{nameof(Product.UnitPrice)} = @UnitPrice, " +
+                    $"{nameof(Product.UnitsInStock)} = @UnitsInStock, " +
+                    $"{nameof(Product.UnitsOnOrder)} = @UnitsOnOrder, " +
+                    $"{nameof(Product.ReorderLevel)} = @ReorderLevel, " +
+                    $"{nameof(Product.Discontinued)} = @Discontinued " +
+                    $"where Id = {product.Id}";
+
+                SetSqlParameters(command.Parameters, product);
+                await connection.OpenAsync();
+                commandExecutionResult = await command.ExecuteNonQueryAsync();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            await connection.DisposeAsync();
+            return commandExecutionResult == null ? false : ((int)commandExecutionResult == 1);
         }
     }
 }
